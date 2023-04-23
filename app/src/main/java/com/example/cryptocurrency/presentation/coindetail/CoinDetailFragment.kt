0e076009ac615b9568342254590f41357521cffe
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2023-04-23 16:57:17 +0200

    add adapter to show team members and add recyclerview to show teams members add some strings

--- app/src/main/java/com/example/cryptocurrency/presentation/coindetail/CoinDetailFragment.kt
+++ app/src/main/java/com/example/cryptocurrency/presentation/coindetail/CoinDetailFragment.kt
@@ -1,21 +1,24 @@
 package com.example.cryptocurrency.presentation.coindetail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.example.cryptocurrency.data.remote.dto.Team
 import com.example.cryptocurrency.databinding.FragmentCoinDetailBinding
-import com.example.cryptocurrency.presentation.CoinListViewModel
 import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CoinDetailFragment : Fragment() {
-    private val viewModle: CoinDetailViewModel by viewModels()
+    private val viewModel: CoinDetailViewModel by viewModels()
+    private var coinDetailAdapter: CoinDetailAdapter? = null
+
 
     private lateinit var binding:FragmentCoinDetailBinding
     override fun onCreateView(
@@ -24,30 +27,43 @@ class CoinDetailFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding= FragmentCoinDetailBinding.inflate(inflater)
-        binding.lifecycleOwner = this
+        setupRecyclerView()
 
-       val coin_id=CoinDetailFragmentArgs.fromBundle(requireArguments())
-        viewModle.getCoin(coin_id.selectedId)
+       val coinId=CoinDetailFragmentArgs.fromBundle(requireArguments())
+        viewModel.getCoin(coinId.selectedId)
 
 
         lifecycleScope.launch{
 
-            viewModle.state.collect{
+            viewModel.state.collect{
                 when {
                     it.error.isNotBlank() -> {
+                        binding.error.visibility=View.VISIBLE
                         binding.error.text=it.error
                         binding.statusLoadingWheel.visibility=View.GONE
+                        binding.TeamMembers.visibility=View.GONE
+                        binding.isActive.visibility=View.GONE
+                        binding.rank.visibility=View.GONE
 
                     }
                     it.isLoading -> {
                         binding.error.visibility=View.GONE
                         binding.statusLoadingWheel.visibility=View.VISIBLE
+                        binding.TeamMembers.visibility=View.INVISIBLE
+                        binding.isActive.visibility=View.INVISIBLE
+                        binding.rank.visibility=View.INVISIBLE
+
+
 
                     }
                     else -> {
                         binding.error.visibility=View.GONE
                         binding.coinDetail= it.coin!!
                         addChips(it.coin.tags)
+                        bindListOfTeam(it.coin.team)
+                        binding.TeamMembers.visibility=View.VISIBLE
+                        binding.rank.visibility=View.VISIBLE
+                        binding.isActive.visibility=View.VISIBLE
                         binding.statusLoadingWheel.visibility=View.GONE
 
 
@@ -59,8 +75,22 @@ class CoinDetailFragment : Fragment() {
         return binding.root
     }
 
+    private fun setupRecyclerView() {
+        coinDetailAdapter= CoinDetailAdapter()
+        binding.teamRecycler.adapter = coinDetailAdapter
+    }
+    private fun bindListOfTeam(list: List<Team>){
+        if(list.isEmpty()){
+            binding.teamRecycler.visibility=View.GONE
+            binding.TeamMembers.visibility=View.GONE
+        }else{
+            coinDetailAdapter?.submitList(list)
+            Log.v("team", list.toString())
+        }
 
-    fun addChips(list: List<String>){
+    }
+
+    private fun addChips(list: List<String>){
         for(i in list){
             val chip=Chip(this.context)
             chip.text= i
